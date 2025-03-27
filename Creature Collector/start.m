06094766fdf7055175d4fbbe3a650dec1245775d
DaChickenGamer <rjmichalski2006@gmail.com>
2025-03-26 22:35:41 -0400

    Grass Generated Randomly
    The grass is now randomly generated when the player first spawns. In addition, the background is now brown

--- Creature Collector/start.m
+++ Creature Collector/start.m
@@ -4,7 +4,7 @@ close all
 
 
 % Initialize scene
-my_scene = simpleGameEngine('assets/retro_pack.png',16,16,4);
+my_scene = simpleGameEngine('assets/retro_pack.png',16,16,4, [109, 76, 65]);
 
 %% Initialize Variables
 gameRunning = true;
@@ -16,33 +16,32 @@ gameRunning = true;
 % Set up variables to name the various sprites
 % The formula here is: (row-1)*32 + column
 blank_sprite = 1;
-sword_sprite = 28*32 + 3;
-boots_sprite = 23*32 + 9;
-shield_sprite = 26*32 + 9;
-potion_sprite = 23*32 + 27;
-monster_sprite = 9*32 + 19;
 player_sprite = 27;
-door_sprite = 9*32 + 11;
 empty_room_sprite = 17;
+grass_sprite = 65;
 
 %% Initalize Board
 % Display empty board   
-rooms_display = empty_room_sprite * ones(10,10);
+rooms_display = blank_sprite * ones(10,10);
 drawScene(my_scene,rooms_display)
 
 % Now set up A second layer with the game pieces
-gameboard_display = blank_sprite * ones(10,10);
-gameboard_display(10,10) = door_sprite;
-gameboard_display(5,5) = sword_sprite;
-gameboard_display(3,4) = shield_sprite;
-gameboard_display(6,9) = boots_sprite;
-gameboard_display(2,8) = potion_sprite;
+gameboard_display = ones(10, 10);
 
-gameboard_display(3,7) = monster_sprite;
-gameboard_display(8,8) = monster_sprite;
-gameboard_display(7,3) = monster_sprite;
+for x = 1:10
+    for y = 1:10
+        randomNum = randi(2);
+        if (randomNum == 1)
+            gameboard_display(y, x) = grass_sprite;
+        else
+            gameboard_display(y, x) = blank_sprite;
+        end
+    end
+end
 
 drawScene(my_scene,rooms_display,gameboard_display)
+
+
 
 %% Initalize Player
 player = player(player_sprite, 2, 2, my_scene, rooms_display, gameboard_display);
